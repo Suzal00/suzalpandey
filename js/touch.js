35616eb0fc4924844cb428b3da107317dce965c1
Suzal00 <137767064+Suzal00@users.noreply.github.com>
2023-07-30 17:22:17 +0545

    Update touch.js

--- js/touch.js
+++ js/touch.js
@@ -3,6 +3,10 @@ const colors = ["#00C2FF", "#0072FF", "#FF0063", "#FFBB00", "#FF5733", "#00FFAA"
 let animateCloudBallFragment = document.createDocumentFragment();
 
 function animateCloudBall(event) {
+  if (event.pointerType === "touch") {
+    event.preventDefault(); // Prevent scrolling interference on touch devices
+  }
+
   let cloudBall = document.createElement("div");
   cloudBall.classList = "cloud-ball";
   animateCloudBallFragment.appendChild(cloudBall);
@@ -11,13 +15,8 @@ function animateCloudBall(event) {
   let color = colors[Math.floor(Math.random() * colors.length)];
   cloudBall.style.background = `radial-gradient(circle, ${color}, #EAF6F6)`;
 
-  if (event.type === "mousemove") {
-    cloudBall.style.left = event.clientX - 10 + "px";
-    cloudBall.style.top = event.clientY - 10 + "px";
-  } else if (event.type === "touchmove") {
-    cloudBall.style.left = event.touches[0].clientX - 10 + "px";
-    cloudBall.style.top = event.touches[0].clientY - 10 + "px";
-  }
+  cloudBall.style.left = event.clientX - 10 + "px";
+  cloudBall.style.top = event.clientY - 10 + "px";
 
   setTimeout(() => {
     cloudBall.style.opacity = 0;
@@ -27,11 +26,4 @@ function animateCloudBall(event) {
   }, 300); // Time (in milliseconds) to wait before making the cloud ball disappear
 }
 
-document.addEventListener("mousemove", animateCloudBall);
-
-// Touch events for mobile devices
-document.addEventListener("touchmove", (event) => {
-  // Prevent default touchmove behavior to avoid scrolling interference
-  event.preventDefault();
-  animateCloudBall(event);
-});
+document.addEventListener("pointermove", animateCloudBall);
