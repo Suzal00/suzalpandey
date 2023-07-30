b50285ce2e7c077a697549a9fc0f308708cad838
Suzal00 <137767064+Suzal00@users.noreply.github.com>
2023-07-30 16:54:42 +0545

    Add files via upload

---
+++ js/touch.js
@@ -0,0 +1,25 @@
+const colors = ["#00C2FF", "#0072FF", "#FF0063", "#FFBB00", "#FF5733", "#00FFAA"];
+
+let animateCloudBallFragment = document.createDocumentFragment();
+
+function animateCloudBall(event) {
+  let cloudBall = document.createElement("div");
+  cloudBall.classList = "cloud-ball";
+  animateCloudBallFragment.appendChild(cloudBall);
+  document.body.appendChild(animateCloudBallFragment);
+
+  let color = colors[Math.floor(Math.random() * colors.length)];
+  cloudBall.style.background = `radial-gradient(circle, ${color}, #EAF6F6)`;
+
+  cloudBall.style.left = event.clientX - 10 + "px";
+  cloudBall.style.top = event.clientY - 10 + "px";
+
+  setTimeout(() => {
+    cloudBall.style.opacity = 0;
+    setTimeout(() => {
+      document.body.removeChild(cloudBall);
+    }, 150); // Time (in milliseconds) to wait before removing the element
+  }, 150); // Time (in milliseconds) to wait before making the cloud ball disappear
+}
+
+document.addEventListener("mousemove", animateCloudBall);
