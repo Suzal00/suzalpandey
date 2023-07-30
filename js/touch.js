ad23061825f0e966211064df6396017dfc2e57f8
Suzal00 <137767064+Suzal00@users.noreply.github.com>
2023-07-30 17:29:28 +0545

    Update touch.js

--- js/touch.js
+++ js/touch.js
@@ -3,6 +3,10 @@ const colors = ["#00C2FF", "#0072FF", "#FF0063", "#FFBB00", "#FF5733", "#00FFAA"
 let animateCloudBallFragment = document.createDocumentFragment();
 
 function animateCloudBall(event) {
+  if (event.pointerType === "touch") {
+    event.preventDefault(); // Prevent scrolling interference on touch devices
+  }
+
   let cloudBall = document.createElement("div");
   cloudBall.classList = "cloud-ball";
   animateCloudBallFragment.appendChild(cloudBall);
@@ -11,15 +15,8 @@ function animateCloudBall(event) {
   let color = colors[Math.floor(Math.random() * colors.length)];
   cloudBall.style.background = `radial-gradient(circle, ${color}, #EAF6F6)`;
 
-  if (event.type === "mousemove") {
-    cloudBall.style.left = event.clientX - 10 + "px";
-    cloudBall.style.top = event.clientY - 10 + "px";
-  } else if (event.type === "touchmove" || event.type === "scroll") {
-    let touchX = event.clientX || event.touches[0].clientX;
-    let touchY = event.clientY || event.touches[0].clientY;
-    cloudBall.style.left = touchX - 10 + "px";
-    cloudBall.style.top = touchY - 10 + "px";
-  }
+  cloudBall.style.left = event.clientX - 10 + "px";
+  cloudBall.style.top = event.clientY - 10 + "px";
 
   setTimeout(() => {
     cloudBall.style.opacity = 0;
@@ -29,10 +26,14 @@ function animateCloudBall(event) {
   }, 300); // Time (in milliseconds) to wait before making the cloud ball disappear
 }
 
-document.addEventListener("mousemove", animateCloudBall);
+document.addEventListener("pointermove", animateCloudBall);
 
 // Touch events for mobile devices
-document.addEventListener("touchmove", animateCloudBall);
+document.addEventListener("touchstart", (event) => {
+  animateCloudBall(event);
+});
 
-// Scroll events for mobile devices
-document.addEventListener("scroll", animateCloudBall);
+document.addEventListener("touchmove", (event) => {
+  event.preventDefault(); // Prevent scrolling interference on touch devices
+  animateCloudBall(event);
+});
