e2d2062bac7cd385146a5365d087d73208db83b1
Suzal00 <137767064+Suzal00@users.noreply.github.com>
2023-07-30 17:05:47 +0545

    Update touch.js

--- js/touch.js
+++ js/touch.js
@@ -11,15 +11,21 @@ function animateCloudBall(event) {
   let color = colors[Math.floor(Math.random() * colors.length)];
   cloudBall.style.background = `radial-gradient(circle, ${color}, #EAF6F6)`;
 
-  cloudBall.style.left = event.clientX - 10 + "px";
-  cloudBall.style.top = event.clientY - 10 + "px";
+  if (event.type === "mousemove") {
+    cloudBall.style.left = event.clientX - 10 + "px";
+    cloudBall.style.top = event.clientY - 10 + "px";
+  } else if (event.type === "touchmove") {
+    cloudBall.style.left = event.touches[0].clientX - 10 + "px";
+    cloudBall.style.top = event.touches[0].clientY - 10 + "px";
+  }
 
   setTimeout(() => {
     cloudBall.style.opacity = 0;
     setTimeout(() => {
       document.body.removeChild(cloudBall);
-    }, 150); // Time (in milliseconds) to wait before removing the element
-  }, 150); // Time (in milliseconds) to wait before making the cloud ball disappear
+    }, 200); // Time (in milliseconds) to wait before removing the element
+  }, 300); // Time (in milliseconds) to wait before making the cloud ball disappear
 }
 
 document.addEventListener("mousemove", animateCloudBall);
+document.addEventListener("touchmove", animateCloudBall);
